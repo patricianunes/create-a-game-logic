641350ce631c128f90e03829b9016ed1b5c5cbaf
Patricia Nunes <patricia@krint.io>
2019-02-13 14:27:03 +1100

    create game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+console.log("User flippped" + cardOne);
+
+console.log("User flipped" + cardTwo);
+
