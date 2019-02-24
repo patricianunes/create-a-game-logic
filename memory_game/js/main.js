cacd8275b47b0dee27214d0fd105d676941d6758
Patricia Nunes <patricia@krint.io>
2019-02-24 21:18:17 +1100

    create a game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,18 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
-console.log("User flippped" + cardOne);
+cardInPlay.push(cardOne);
+cardInPlay.push(cardTwo);
 
-console.log("User flipped" + cardTwo);
-
+if (cardInPlay.length === 2) {
+  if (cardInPlay[0] === cardInPlay[1]) {
+    alert("You found a match.");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
+  
+console.log("User flipped" + " " + cardOne);
+console.log("User flipped" + " " + cardTwo);
