ccc9f4084108ae115f5017f05a3f310db5c5514f
Patricia Nunes <patricia@krint.io>
2019-02-26 17:58:51 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,33 @@
+//array with all cards
 var cards = ["queen", "queen", "king", "king"];
-var cardInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
+//array with cards that are in play
+var cardsInPlay = [];
 
-cardInPlay.push(cardOne);
-cardInPlay.push(cardTwo);
-
-if (cardInPlay.length === 2) {
-  if (cardInPlay[0] === cardInPlay[1]) {
-    alert("You found a match.");
-  } else {
-    alert("Sorry, try again.");
+//function to store the steps to check for a match.
+var checkForMatch = function () {
+  if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+    } else {
+    console.log("Sorry, try again.");
+    }
   }
-}
-  
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardTwo);
+};
+
+//function to store all steps that should happen when the user flips a card.
+var flipCard = function (cardId) {
+
+  //push the cards that the user flipped to the cardsInPlay array
+  cardsInPlay.push(cardId)
+
+  //test to see if the code is run
+  console.log("User flipped " + cardId)
+
+  //call the function to check for a match
+  checkForMatch ();
+};
+
+//call the function and give then two values in the array cards  
+flipCard (cards[0]);
+flipCard (cards[2]);
+
