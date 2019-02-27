c9e5d8aa7188bce9ce045bda82d87bf3e3652fbd
Patricia Nunes <patricia@krint.io>
2019-02-27 23:23:20 +1100

    create a game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,33 +1,55 @@
 //array with all cards
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
+
 //array with cards that are in play
 var cardsInPlay = [];
 
 //function to store the steps to check for a match.
 var checkForMatch = function () {
-  if (cardsInPlay.length === 2) {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     console.log("You found a match!");
     } else {
     console.log("Sorry, try again.");
     }
-  }
 };
 
 //function to store all steps that should happen when the user flips a card.
 var flipCard = function (cardId) {
 
   //push the cards that the user flipped to the cardsInPlay array
-  cardsInPlay.push(cardId)
+  cardsInPlay.push(cards[cardId].rank)
 
   //test to see if the code is run
-  console.log("User flipped " + cardId)
+  console.log("User flipped " + cards[cardId].rank)
+  console.log("User flipped " + cards[cardId].cardImage)
+  console.log("User flipped " + cards[cardId].suit)
 
   //call the function to check for a match
   checkForMatch ();
 };
 
 //call the function and give then two values in the array cards  
-flipCard (cards[0]);
-flipCard (cards[2]);
+flipCard (0);
+flipCard (2);
 
