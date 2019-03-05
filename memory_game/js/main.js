6a7c10b3b26cc9f6ac6515c45766022ab8da4eb9
Patricia Nunes <patricia@krint.io>
2019-03-05 13:09:42 +1100

    crete a game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,29 +27,43 @@ var cardsInPlay = [];
 
 //function to store the steps to check for a match.
 var checkForMatch = function () {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
-    } else {
-    console.log("Sorry, try again.");
-    }
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+      } else {
+      alert("Sorry, try again.");
+      }
+  };
 };
 
+
 //function to store all steps that should happen when the user flips a card.
-var flipCard = function (cardId) {
+var flipCard = function () {
+  var cardId = this.getAttribute('data-id');
+  this.setAttribute('src', cards[cardId].cardImage);
 
   //push the cards that the user flipped to the cardsInPlay array
   cardsInPlay.push(cards[cardId].rank)
 
   //test to see if the code is run
   console.log("User flipped " + cards[cardId].rank)
-  console.log("User flipped " + cards[cardId].cardImage)
-  console.log("User flipped " + cards[cardId].suit)
+  console.log(cards[cardId].cardImage)
+  console.log(cards[cardId].suit)
 
   //call the function to check for a match
   checkForMatch ();
 };
 
-//call the function and give then two values in the array cards  
-flipCard (0);
-flipCard (2);
+//function new game board
+var createBoard = function () {
+for (var i = 0; i < cards.length; i++){
+  var cardElement = document.createElement('img');
+  cardElement.setAttribute('src', 'images/back.png');
+  cardElement.setAttribute('data-id', i);
+  cardElement.addEventListener('click', flipCard);
+  document.getElementById('game-board').appendChild(cardElement);
+}
+};
 
+//call the function game board
+createBoard();  
